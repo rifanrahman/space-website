2c34e8671b51f3519cf961d70e3966c02e695f19
unknown <rifanrahman8817@gmail.com>
2023-12-27 16:52:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+ // JavaScript code to toggle the menu
+ const menuToggle = document.querySelector('.menu-toggle');
+ const nav = document.querySelector('nav ul');
+
+ menuToggle.addEventListener('click', () => {
+     nav.classList.toggle('active');
+ });
